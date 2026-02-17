8ba727c864e6963c7cd15c6c72f860c6ed7dc495
Adrian Fayos <adrianfayos1234@gmail.com>
2026-02-17 17:49:10 -0300

    create itemCounter

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,32 +1,51 @@
+import { useState } from 'react'
+
+// import './ItemCounter.css'
+import styles from './ItemCounter.module.css'
+
 interface Props {
   name: string
   quantity?: number
 }
 
-export const ItemCounter = ({ name, quantity }: Props) => {
-  const handleClick = () => {
-    console.log(`Click en ${name}`)
+export const ItemCounter = ({ name, quantity = 0 }: Props) => {
+  const [count, setCount] = useState(quantity)
+
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+
+  const handleSub = () => {
+    if (count === 0) return
+    setCount(count - 1)
+  }
+
+  const handleDel = () => {
+    setCount(0)
   }
 
   return (
     <section
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 10,
-        marginTop: 10,
-      }}
+      className={styles.itemRow}
+      // style={{
+      //   display: 'flex',
+      //   alignItems: 'center',
+      //   gap: 10,
+      //   marginTop: 10,
+      // }}
     >
       <span
-        style={{
-          width: 150,
-        }}
+        className={styles['item-text']}
+        // style={{
+        //   width: 150,
+        // }}
       >
         {name}
       </span>
-      <button onClick={handleClick}>+1</button>
-      <span>{quantity}</span>
-      <button>-1</button>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleSub}>-1</button>
+      <button onClick={handleDel}>🗑️</button>
     </section>
   )
 }
